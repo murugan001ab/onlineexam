d44e891d0512fab224ff826a689c4c7ecd74e815
murugan001ab <murugan001ab@gmail.com>
2026-08-14 14:20:36 +0530

    first commit

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -5,7 +5,7 @@ load_dotenv()
 
 
 class Settings:
-    DB=os.getenv("DB","postgresql+psycopg://avnadmin:AVNS_gZR0OqN1cYI7PajU3sX@pg-8ad667a-murugan001ac-ad40.f.aivencloud.com:19573/exam?sslmode=require")
+    DB=os.getenv("DB","")
 
 
 
